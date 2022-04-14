fab72f3c8b66992b95fed1b88b9df1e101886f0b
FranGutierrez <55754815+FranGutierrez@users.noreply.github.com>
2022-04-14 16:26:09 -0300

    BaSP-2022: Exercise 01 solved

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,30 @@
+console.log('EXCERCISE 1: VARIABLES AND OPERATORS');
+
+/* a. Crear dos variables numéricas y utilizar el operador suma 
+para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+var num1 = 15;
+var num2 = 23;
+
+var amount = num1 + num2;
+
+console.log(amount);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var name = 'Francisco';
+var surname = 'Gutierrez';
+
+var fullName = name + ' ' + surname;
+
+console.log(fullName);
+
+/* c. Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+var txt1 = 'Radium';
+var txt2 = 'Rocket';
+
+var letters = txt1.length + txt2.length;
+
+console.log(letters);
